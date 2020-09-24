f45278b60f395c7a9a8392e1e0404d9c74f5dcfc
Greg Melillo <gregmelillo@Gregs-MacBook-Pro-490.local>
2020-09-23 22:48:32 -0400

    r

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-/* const axios = require('axios').default;
+const axios = require('axios').default;
 
 const { pastLaunchQuery, upcomingLaunchesQueryMin, pastLaunchesQueryMin } = require('./queries/launches')
 const LAUNCHES_QUERY = 'https://api.spacexdata.com/v4/launches/query'
- */
+
 
 const app = express();
 
@@ -17,6 +17,25 @@ app.get('/api/passwords', (req, res) => {
   const count = 5;
 
   console.log(`Sent ${count} passwords`);
+});
+
+app.post('/api/launches/past', (req, res, next) => {
+    axios.post(LAUNCHES_QUERY, pastLaunchesQueryMin)
+       .then(resp => res.send(resp.data))
+       .catch(err => res.secn(err));
+});
+
+app.post('/api/launches/upcoming', (req, res, next) => {
+    axios.post(LAUNCHES_QUERY, upcomingLaunchesQueryMin)
+       .then(resp => res.send(resp.data))
+       .catch(err => res.secn(err));
+});
+
+app.post('/api/launches/single', (req, res, next) => {
+    console.log("param: ", req.body)
+    axios.post(LAUNCHES_QUERY, pastLaunchQuery(req.body.flight_number))
+       .then(resp => res.json(resp.data))
+       .catch(err => res.secn(err));
 });
 
 
